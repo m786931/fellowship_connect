b4c6b8d010a30f18624544e1d9a0296c3177e956
m786931 <Dan.J.McCool@gmail.com>
2025-03-12 20:09:21 -0500

    Changing padding

--- app/welcome/[churchId]/connect_card.tsx
+++ app/welcome/[churchId]/connect_card.tsx
@@ -46,8 +46,8 @@ console.log("ServerState: " + serverState.successMsg)
     };
   
   return (
-    <div className='p-0'>
-      <form action={formAction} ref={formRef} className="w-full mx-auto p-1 bg-white rounded-lg shadow-md">
+    <div className='pt-20 px-3'>
+      <form action={formAction} ref={formRef} className="max-w mx-auto p-1 bg-white rounded-lg shadow-md">
         <div className="flex flex-col gap-y-2">
           <div>
             <div className="flex flex-row gap-x-4">
